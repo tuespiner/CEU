08265ed8c5ae96297620929ab855b2465c9e89b7
tuespiner <juanspiner89@gmail.com>
2024-01-08 20:14:49 +0100

    nueva base de datos

--- BBDD/SQL/root.sql
+++ BBDD/SQL/root.sql
@@ -1,6 +1,6 @@
 alter session set "_ORACLE_SCRIPT"=true;
 drop user examen cascade;
-create user Ejercicio1_caladeros identified by a123456
+create user Tema_4 identified by a123456
 default tablespace system
 quota unlimited on system;
-grant connect, create session, create table TO Ejercicio1_caladeros;
+grant connect, create session, create table TO Tema_4;
