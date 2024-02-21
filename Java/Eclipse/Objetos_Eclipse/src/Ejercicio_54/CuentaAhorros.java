14ec041d6aadcfad158d73a2e1797fcca57a80c3
tuespiner <juanspiner89@gmail.com>
2024-02-22 00:33:54 +0100

    fgfdgdfgfdg

--- Java/Eclipse/Objetos_Eclipse/src/Ejercicio_54/CuentaAhorros.java
+++ Java/Eclipse/Objetos_Eclipse/src/Ejercicio_54/CuentaAhorros.java
@@ -10,27 +10,31 @@ import java.util.List;
 public class CuentaAhorros {
 	private String iban;
 	private List<String> listaOperaciones;
+	private BigDecimal total;
 	
 	public CuentaAhorros(String ibn) {
 		this.iban = ibn;
 		this.listaOperaciones = new ArrayList<>();
+		this.total = BigDecimal.ZERO;
 	}
 	
 	public void añadirMovimiento(Movimientos operacion) {
 		this.listaOperaciones.add(operacion.toString());
+		if(operacion.toString().startsWith("[C")|| operacion.toString().startsWith("[R")) {
+			this.total = total.subtract(operacion.getImporte());
+		}else if(operacion.toString().startsWith("[I")) {
+			this.total = total.add(operacion.getImporte());
+		}
 	}
 	
 	public String getTotal() {
-		BigDecimal total= BigDecimal.ZERO;
 		DecimalFormat formato = new DecimalFormat("#,##0.00");
-		Iterator it = listaOperaciones.iterator();
-		while(it.hasNext()) {
-			if(it.next().toString().startsWith("[R")) {
-				Retirada retirada = new Retirada();
-				total = total.subtract(it.next());
-			}
+		if(this.total.doubleValue() < 0) {
+			this.total = BigDecimal.ZERO;
+			return formato.format(this.total.setScale(2, RoundingMode.HALF_DOWN));
+		}else {
+			return formato.format(this.total.setScale(2, RoundingMode.HALF_DOWN));
 		}
-		return formato.format(total.setScale(2, RoundingMode.HALF_DOWN));
 	}
 	
 	public List<String> getMovimientos(){
@@ -38,10 +42,15 @@ public class CuentaAhorros {
 	}
 	public List<String> getRetiradas() {
 		List<String> retiradas= new ArrayList<>();
-		Iterator<String> it = this.listaOperaciones.iterator();
+		/*Iterator<String> it = this.listaOperaciones.iterator();
 		while(it.hasNext()) {
 			if(it.next().startsWith("[R")) {
 				retiradas.add(it.next());
+			}
+		}*/
+		for(String movimiento : this.listaOperaciones) {
+			if(movimiento.startsWith("[R")) {
+				retiradas.add(movimiento);
 			}
 		}
 		return retiradas;
@@ -49,10 +58,15 @@ public class CuentaAhorros {
 	
 	public List<String> getIngresos() {
 		List<String> retiradas= new ArrayList<>();
-		Iterator<String> it = this.listaOperaciones.iterator();
+		/*Iterator<String> it = this.listaOperaciones.iterator();
 		while(it.hasNext()) {
 			if(it.next().startsWith("[I")) {
 				retiradas.add(it.next());
+			}
+		}*/
+		for(String movimiento : this.listaOperaciones) {
+			if(movimiento.startsWith("[I")) {
+				retiradas.add(movimiento);
 			}
 		}
 		return retiradas;
@@ -60,10 +74,15 @@ public class CuentaAhorros {
 	
 	public List<String> getCargos() {
 		List<String> retiradas= new ArrayList<>();
-		Iterator<String> it = this.listaOperaciones.iterator();
+		/*Iterator<String> it = this.listaOperaciones.iterator();
 		while(it.hasNext()) {
 			if(it.next().startsWith("[C")) {
 				retiradas.add(it.next());
+			}
+		}*/
+		for(String movimiento : this.listaOperaciones) {
+			if(movimiento.startsWith("[C")) {
+				retiradas.add(movimiento);
 			}
 		}
 		return retiradas;
