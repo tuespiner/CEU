aa590178f2d8f6efe2798aa201d3493b2fb0ec4c
tuespiner <juanspiner89@gmail.com>
2024-05-13 23:28:43 +0200

    fjdkañls

---
+++ BBDD/SQL/EjerciciosEstudio.sql
@@ -0,0 +1,15 @@
+/*Aqui voy a ponerme a estudiar, a ver si consigo aprobar bbdd mañana, 
+primero voy a escribir como se escriben todas las cosas que hemos visto 
+y una pequeña descripcion de como funciona,para que sirve etc.
+(cosas como funciones, etc solo voh a poner como se escribe)*/
+
+/*FUNCIONES*/
+create or replace function nombreFuncion() return number
+is
+begin
+end;
+/
+
+/*PROCEDURE*/
+
+create or replace 
