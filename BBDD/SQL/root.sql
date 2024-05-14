564c93319a77d9e176278213edf489617318b696
tuespiner <juanspiner89@gmail.com>
2024-05-14 17:33:08 +0200

    asdfg

--- BBDD/SQL/root.sql
+++ BBDD/SQL/root.sql
@@ -1,10 +1,10 @@
 alter session set "_ORACLE_SCRIPT"=true;
 
 drop user examen cascade;
-create user prueba identified by a123456
+create user Tema6 identified by a123456
 default tablespace system 
 quota unlimited on system;
-grant connect, create session, create table TO prueba;
+grant connect, create session, create table TO Tema6;
 
 
-grant create procedure to prueba;
+grant create procedure to Tema6;
