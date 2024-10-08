dd815e66cc8f43998b374c0790da7652a154d96e
tuespiner <juanspiner89@gmail.com>
2024-10-08 21:02:09 +0200

    mensaje

---
+++ segundo/Servidor (Springboot)/workspace-spring-tool-suite-4-4.25.0.RELEASE/Ejercicio11/src/main/java/com/example/demo/modelo/Alumnos.java
@@ -0,0 +1,58 @@
+package com.example.demo.modelo;
+
+public class Alumnos {
+	private String id;
+	private String nombre;
+	private String email;
+	private int edad;
+	private Direccion direccion;
+	
+	public Alumnos(String id, String nombre,String email, int edad) {
+		this.id = id;
+		this.nombre = nombre;
+		this.email = email;
+		this.edad = edad;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+}
---
+++ segundo/Servidor (Springboot)/workspace-spring-tool-suite-4-4.25.0.RELEASE/Ejercicio11/src/main/java/com/example/demo/modelo/Direccion.java
@@ -0,0 +1,28 @@
+package com.example.demo.modelo;
+
+public class Direccion {
+	private String calle;
+	private Integer codigoPostal;
+	private String ciudad;
+	
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public Integer getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(Integer codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+	
+}
