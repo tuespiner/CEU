b994c51092d25f921b1e7a542e2119d35f948c7d
tuespiner <juanspiner89@gmail.com>
2024-10-14 00:20:42 +0200

    subo lo del dia 14

---
+++ proyectitos/script.js
@@ -0,0 +1,49 @@
+function crearEstrella(color){
+    var x = parseInt(Math.random()*(100));
+    var y = parseInt(Math.random()*(100));
+    let cadSombra = `${x}vw ${y}vh 0px ${color}`;
+    return cadSombra;
+}
+function crearEstrellas(color = "white"){
+    let cadSombra = "";
+    let cantEstrellas = 400;
+    for(let i = 0; i <= cantEstrellas; i++){
+        if(i <= (cantEstrellas-1)){
+            cadSombra = cadSombra + crearEstrella(color) + ",";
+        }else{
+            cadSombra = cadSombra + crearEstrella(color) ;
+        }
+    }
+    console.log(cadSombra)
+    return cadSombra;
+}
+function capaEstrella(num, top = -100){
+    var estrella = document.getElementById(`estrellas${num}`);
+    estrella.style.boxShadow = crearEstrellas();
+    estrella.animate([{marginTop:`${top}vh`},{marginTop:`100vh`}],{duration:10000,},);
+    if(num == 3){
+        var dysplaynone = () =>{
+            estrella.style.display = "none"
+        }
+        setTimeout(dysplaynone,4900);
+    }
+}
+
+function main(){
+    var contador = 1;
+    var cpb = () =>{//continious playback background ese es el nombre jajaja
+        if(contador == 1){
+            capaEstrella(2);
+            contador = 2;
+        }else if(contador == 2){
+            capaEstrella(1);
+            contador = 1;
+        }
+       
+    }
+    capaEstrella(3,0);
+    capaEstrella(1);
+    setInterval(cpb,5000);
+    
+}
+main();
