8d3600aa3d9107805176b36e573f8a224e374721
tuespiner <juanspiner89@gmail.com>
2024-10-28 15:25:31 +0100

    hjola

--- Java/Eclipse/tuCuloGordo/src/tuCuloGordo/App.java
+++ Java/Eclipse/tuCuloGordo/src/tuCuloGordo/App.java
@@ -5,25 +5,8 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Boolean buleano = false;
-		while(buleano == false) {
-			System.out.println("Dime tu edad");
-			int edad = sc.nextInt();
-			if (edad >= 18) {
-				System.out.println("Mayor de edad");
-				if (edad == 100) {
-					System.out.println("Salgo del bucle");
-					buleano = true;
-				}
-			}else if (edad <= 0){
-				System.out.println("No has nacido");
-				
-			}
-			else {
-				System.out.println("Menor de edad");
-			}
-
+		if(1 == "1") {
+			System.out.println("hola");
 		}
 		
 	}
