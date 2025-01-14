150ca05105e1436749d4417043c4afb34e43560d
tuespiner <juanspiner89@gmail.com>
2025-01-14 15:51:06 +0100

    hola

---
+++ segundo/Servidor (php)/Trabajo final php/Login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/loginStyle.css">
+    <title>Cervecería Juan</title>
+</head>
+<body>
+    <section class="login-container">
+        <div class="login">
+            <h1>Iniciar sesión</h1>
+            <form action="index.php">
+                <label for="user">Usuario: </label>
+                <input type="text" name="user" id="user">
+                <label for="passwd">Contraseña: </label>
+                <input type="text" name="passwd" id="passwd">
+            </form>
+        </div>
+        <div class="signUp">
+            <h1>Registrate</h1>
+        </div>
+    </section>
+</body>
+</html>
