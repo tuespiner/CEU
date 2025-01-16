cf7fb14ec470f88e8bb3c818ffa696e0c044c478
tuespiner <juanspiner89@gmail.com>
2025-01-16 19:05:16 +0100

    Hola

--- segundo/diseño/jquery/EjercicioAvanzado/js/script.js
+++ segundo/diseño/jquery/EjercicioAvanzado/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.login, .signup').hide()
     $('#inicia').on('click', function(){
         $('.login-signup, .login').toggle()
+        $('#titulo').text('CUENTA')
     })
     $('#registra').on('click', function(){
         $('.login-signup, .signup').toggle()
