f55827f20a8876d08c81dcbf6887c585c7c1b847
tuespiner <juanspiner89@gmail.com>
2025-02-03 17:57:04 +0100

    hola

--- segundo/diseño/serpiente/script/script.js
+++ segundo/diseño/serpiente/script/script.js
@@ -27,16 +27,29 @@ var cuerpoSerpiente = [
         key: null
     }
 ]
-var recordedKey = 'left'
-
+var interval
+var recordedKey = 'up'
 $(document).ready(function(){
-    var interval = setInterval(ini, 333)
+    $(document).on('keydown', function(event){
+        detecBotones(event)
+    })
+    interval = setInterval(ini, 60)
 })
 function ini(){
     pintar()
     moverse()
 }
-
+function detecBotones(event ){
+    if(event.key == 'ArrowUp' || event.key == 'w'){
+        recordedKey = 'up'
+    }else if(event.key == 'ArrowDown' || event.key == 's'){
+        recordedKey = 'down'
+    }else if(event.key == 'ArrowLeft' || event.key == 'a'){
+        recordedKey = 'left'
+    }else if(event.key == 'ArrowRight' || event.key == 'd'){
+        recordedKey = 'right'
+    }
+}
 
 function pintar(){
     $('#selpiente').text('')
@@ -51,28 +64,45 @@ function pintar(){
 
 
 function moverse(){
-    let posAnterior
-    let posNueva = 0
-    console.log('mama')
-    for(let i = 0; i < cuerpoSerpiente.length; i++){
-        console.log('hola')
-        if(cuerpoSerpiente[i - 1] != undefined){
-                posAnterior = cuerpoSerpiente[i -1]
-                cuerpoSerpiente[i -1] = cuerpoSerpiente[i]
-                console.log('hoila')
-            if(i == 0){
-                if(recordedKey == 'top'){
-                    cuerpoSerpiente[i].top = cuerpoSerpiente[i].top - 10
-                }else if(recordedKey == 'left'){
-                    cuerpoSerpiente[i].left = cuerpoSerpiente[i].left - 10
-                }else if(recordedKey == 'right'){
-                    cuerpoSerpiente[i].left = cuerpoSerpiente[i].left + 10
-                }else if(recordedKey == 'bottom'){
-                    cuerpoSerpiente[i].top = cuerpoSerpiente[i].top + 10
-                }
-            }
-        }
-        
+    let posAnteriorTop
+    let posAnteriorLeft
+    let posNuevaTop = cuerpoSerpiente[0].top
+    let posNuevaLeft = cuerpoSerpiente[0].left
+    if(recordedKey == 'up'){
+        cuerpoSerpiente[0].top = cuerpoSerpiente[0].top - 10
+    }else if(recordedKey == 'left'){
+        cuerpoSerpiente[0].left = cuerpoSerpiente[0].left - 10
+    }else if(recordedKey == 'right'){
+        cuerpoSerpiente[0].left = cuerpoSerpiente[0].left + 10
+    }else if(recordedKey == 'down'){
+        cuerpoSerpiente[0].top = cuerpoSerpiente[0].top + 10
     }
+    for(let i = 0; i < cuerpoSerpiente.length; i++){
+        posAnteriorTop = cuerpoSerpiente[i + 1].top
+        posAnteriorLeft = cuerpoSerpiente[i + 1].left
+        if(cuerpoSerpiente[i + 1] != undefined){
+           cuerpoSerpiente[i + 1].top = posNuevaTop 
+           cuerpoSerpiente[i + 1].left = posNuevaLeft
+        }else{
+            cuerpoSerpiente[i].top = posNuevaTop 
+            cuerpoSerpiente[i].left = posNuevaLeft
+        }
+        posNuevaTop = posAnteriorTop
+        posNuevaLeft = posAnteriorLeft
+        if(cuerpoSerpiente[0].top == 200){
+            console.log('mecaguen10')
+        }
+        if(cuerpoSerpiente[0].top > 200 ||
+             cuerpoSerpiente[0].top < -200 ||
+              cuerpoSerpiente[0].left > 200 ||
+               cuerpoSerpiente[0].left < -200){
+                fin()
+               }
+    }
+}
+
+function fin(){
+    console.log('mamahuevo')
+    clearInterval(interval)
 }
 
