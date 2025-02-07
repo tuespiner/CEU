850027e427933dd1bcf66edb7e1b61fbc4dd977c
JUAN LÓPEZ  CARMONA <jlopez6832@ceuandalucia.es>
2025-02-07 17:01:25 +0100

    hola

--- segundo/Servidor (Springboot)/workspace-spring-tool-suite-4-4.25.0.RELEASE/Tema5EjerciciosReal/src/main/java/com/example/demo/controller/VistaController.java
+++ segundo/Servidor (Springboot)/workspace-spring-tool-suite-4-4.25.0.RELEASE/Tema5EjerciciosReal/src/main/java/com/example/demo/controller/VistaController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,20 +21,23 @@ public class VistaController {
 	private ClienteService service;
 	
 	@GetMapping("/")
-	public String inicio(Model model) {
+	public String inicio( Model model) {
 		model.addAttribute("clientes", service.getAllClientes());
 		
 		return "index";
 	}
 	
+	@GetMapping("/{id}")
+	public String inicio(@PathVariable Integer id , Model model) {
+		model.addAttribute("clienteDetalle", service.getClienteById(id));
+		return "detallesCliente";
+	}
 
 	
 	@GetMapping("/detallesCliente")
-	public String detallesCliente(  Model model) {
+	public String detallesCliente( Model model) {
 		return "detallesCliente";
 	}
-	
-
 	
 	@GetMapping("/agregarCliente")
 	public String agregarCliente( Model model) {
@@ -43,5 +49,17 @@ public class VistaController {
 	public String agregarCliente(@ModelAttribute Cliente cliente, Model model) {
 		service.saveCliente(cliente);
 		return "redirect:/";
+	}
+	
+	@GetMapping("/buscarPorCiudad")
+	public String buscarPorCiudad(Model model) {
+		return "buscarPorCiudad";
+	}
+	
+	@PostMapping("/buscarPorCiudad")
+	public String buscarPorCiudad(@ModelAttribute String ciudad,  Model model) {
+		model.addAttribute("clientesCiudad", service.findClientesByCiudad(ciudad));
+		model.addAttribute("ciudad", ciudad);
+		return "buscarPorCiudad";
 	}
 }
