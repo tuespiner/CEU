670202e506dae987cb41deb17d5043ab6c00562a
JUAN LÓPEZ  CARMONA <jlopez6832@ceuandalucia.es>
2025-02-14 16:52:27 +0100

    hoal

--- segundo/Servidor (Springboot)/workspace-spring-tool-suite-4-4.25.0.RELEASE/Ejercicio08/src/main/java/com/ej08/controller/UsuarioController.java
+++ segundo/Servidor (Springboot)/workspace-spring-tool-suite-4-4.25.0.RELEASE/Ejercicio08/src/main/java/com/ej08/controller/UsuarioController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ej08.model.Usuario;
 import com.ej08.service.UsuarioService;
@@ -17,6 +20,10 @@ public class UsuarioController {
 	
 	@GetMapping("/")
 	public String index() {
+		return "index";
+	}
+	@GetMapping("/{id}")
+	public String indexId() {
 		return "index";
 	}
 	
@@ -33,8 +40,22 @@ public class UsuarioController {
 		return "usuario-form";
 	}
 	
-	@PostMapping("/usuario-form")
-	public String usuarioForm() {
-		return "redirect: @{/verTodosLosUsuarios}";
+	@GetMapping("/usuario-form/{id}")
+	public String usuarioForm(@PathVariable("id") Integer id ,Model model) {
+		model.addAttribute("usuarioPasado", this.service.getUsuarioById(id));
+		return "usuario-form";
 	}
+	
+	@PostMapping("/usuario-form")
+	public String usuarioForm(@ModelAttribute Usuario usuario, Model model) {
+		this.service.saveUsuario(usuario);
+		return "redirect:/verTodosLosUsuarios";
+	}
+	
+	@GetMapping("/primerUsuario")
+	public String primerUsuario(Model model) {
+		model.addAttribute("usuario", this.service.primerUsuarioDisponible());
+		return "primerUsuario";
+	}
+	
 }
