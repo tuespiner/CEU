3127acc7ed79cf0b417005098fd1aa176d7e5f8b
tuespiner <juanspiner89@gmail.com>
2025-02-21 17:12:35 +0100

    hola

--- segundo/Servidor (php)/CerveceriaFinalT2PHP/paginas/catalogo/catalogoUsuario.php
+++ segundo/Servidor (php)/CerveceriaFinalT2PHP/paginas/catalogo/catalogoUsuario.php
@@ -1,26 +1,13 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/catalogoStyle.css">
-    <title>Cerveceria Juan</title>
-</head>
-<body>
-    <main>
-        <?php
-            while ($fila = $_SESSION['productos']->fetch_assoc()){
-                echo '<div>';
-                echo '<div class="imagen"></div>';
-                echo '<h2>'.$fila['formato'].'</h2>';
-                echo '<ul><li><p>Marca: '.$fila['marca'].'</p></li>';
-                echo '<li><p>Tipo: '.$fila['tipo'].'</p></li>';
-                echo '<li><p>Tamaño: '.$fila['tamano'].'</p></li>';
-                echo '<li><p>Precio: '.$fila['precio'].'€</p></li></ul>';
-                echo '<div><a href="">AÑADIR AL CARRITO</a>';
-                echo '<a href="">VER MÁS</a></div></div>';
-            }
-        ?>
-    </main>
-</body>
-</html>
+<?php
+    while ($fila = $_SESSION['productos']->fetch_assoc()){
+        echo '<div class="producto">';
+        echo '<div class="imagen"><img src="'.$fila['foto'].'"></div>';
+        echo '<div><h2>'.$fila['formato'].'</h2>';
+        echo '<ul><li><p>Marca: '.$fila['marca'].'</p></li>';
+        echo '<li><p>Tipo: '.$fila['tipo'].'</p></li>';
+        echo '<li><p>Tamaño: '.$fila['tamano'].'</p></li>';
+        echo '<li><p>Precio: '.$fila['precio'].'€</p></li></ul>';
+        echo '<div><a href="">AÑADIR AL CARRITO</a>';
+        echo '<a href="">VER MÁS</a></div></div></div>';
+    }
+?>
