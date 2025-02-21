2eda4d05b62a840326cc682138eabc371cb20a01
tuespiner <juanspiner89@gmail.com>
2025-02-21 18:40:32 +0100

    hola

--- segundo/Servidor (php)/CerveceriaFinalT2PHP/paginas/catalogo/catalogoUsuario.php
+++ segundo/Servidor (php)/CerveceriaFinalT2PHP/paginas/catalogo/catalogoUsuario.php
@@ -3,10 +3,10 @@
         echo '<div class="producto">';
         echo '<div class="imagen"><img src="'.$fila['foto'].'"></div>';
         echo '<div><h2>'.$fila['formato'].'</h2>';
-        echo '<ul><li><p>Marca: '.$fila['marca'].'</p></li>';
-        echo '<li><p>Tipo: '.$fila['tipo'].'</p></li>';
-        echo '<li><p>Tamaño: '.$fila['tamano'].'</p></li>';
-        echo '<li><p>Precio: '.$fila['precio'].'€</p></li></ul>';
+        echo '<ul><li><p><span>Marca:</span> '.$fila['marca'].'</p></li>';
+        echo '<li><p><span>Tipo:</span> '.$fila['tipo'].'</p></li>';
+        echo '<li><p><span>Tamaño:</span> '.$fila['tamano'].'</p></li>';
+        echo '<li><p><span>Precio:</span> '.$fila['precio'].'€</p></li></ul>';
         echo '<div><a href="">AÑADIR AL CARRITO</a>';
         echo '<a href="">VER MÁS</a></div></div></div>';
     }
