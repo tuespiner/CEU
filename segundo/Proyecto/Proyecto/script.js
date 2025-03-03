44263193911997c7bb6069e04d3347a4a6df43e9
tuespiner <juanspiner89@gmail.com>
2025-03-03 14:24:21 +0100

    hola

--- segundo/Proyecto/Proyecto/script.js
+++ segundo/Proyecto/Proyecto/script.js
@@ -4,8 +4,25 @@ subir.addEventListener("click", () =>{
 })
 $(document).ready(function(){
     if(sessionStorage.getItem('cookie') != 1){
-        $('.privacidad').show()
+        $('.privacidad').show().slideDown()
         $('body').css('overflow', 'auto')
         $('body').css('overflow-x', 'hidden')
     }
+    $('.aceptar').click(function(){
+        $('.privacidad').hide()
+        $('body').css('overflow', 'auto')
+        $('body').css('overflow-x', 'hidden')
+        sessionStorage.setItem('cookie', 1)
+    })
+    $('.menu-movil').click(function(){
+        $('.click-menu-movil').slideToggle()
+    })
+    $(window).resize(function(){
+        if($(window).width() > 780){
+            $('.click-menu-movil').slideUp()
+        }
+    })
+    $('#cerrar').click(function(){
+        $('.click-menu-movil').slideUp()
+    })
 })
